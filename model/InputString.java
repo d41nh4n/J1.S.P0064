2fa78ecf12252cfc89cb0662c00cab5aac11ab48
d41nh4n <d41nh4n@example.com>
2023-10-21 14:17:53 +0700

    First Commit

---
+++ model/InputString.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Dai Nhan
+ */
+public class InputString {
+    private String input;
+
+    public InputString(String input) {
+        this.input = input;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    @Override
+    public String toString() {
+        return "InputString{" + "input=" + input + '}';
+    }
+    
+    
+}
---
+++ test/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+import controller.CheckInput;
+
+/**
+ *
+ * @author Dai Nhan
+ */
+public class Main {
+    public static void main(String[] args) {
+        CheckInput ci = new CheckInput();
+        ci.run();
+    }
+}
